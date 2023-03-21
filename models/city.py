81b7a677868b6960b5e9619bae00c1cc77ae77b2
Kay Cee <se.kaycee@gmail.com>
2023-03-21 04:03:27 +0000

    feature - connect to mysql via sqlalchemy: update City

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """ The City class, contains state id and name
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
