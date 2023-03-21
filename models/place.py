6d22a4d7f31473b937c94c0811485037cd5f904d
Kay Cee <se.kaycee@gmail.com>
2023-03-21 10:42:28 +0000

    update Amenity and Place: add many-to-many relationship

--- models/place.py
+++ models/place.py
@@ -7,6 +7,16 @@ from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
+
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -34,10 +44,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_id = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         @property
         def reviews(self):
@@ -54,3 +68,15 @@ class Place(BaseModel, Base):
                 if (item.place_id == self.id):
                     result.append(item)
             return (result)
+
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
+
