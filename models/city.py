a767315c3f7b2872c14e9cbfdf91c32ef4cb4253
Kay Cee <se.kaycee@gmail.com>
2023-03-21 05:06:28 +0000

    update City

--- models/city.py
+++ models/city.py
@@ -3,7 +3,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
+from models.place
 
 
 class City(BaseModel):
@@ -15,3 +17,5 @@ class City(BaseModel):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
