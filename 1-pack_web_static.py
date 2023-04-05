1e83e82f5e25a21fd5f273362e3983dc754e8d21
Kay Cee <se.kaycee@gmail.com>
2023-04-05 21:04:36 +0100

    [Feature] add 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Form a tarball from web_static and download to versions '''
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    try:
+        if not os.path.exists('versions'):
+            os.mkdir('versions')
+        t = datetime.now()
+        f = '%Y%m%d%H%M%S'
+        tb_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(tb_path))
+        return tb_path
+    except Exception:
+        return None
