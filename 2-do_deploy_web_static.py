8cf0cffd6c3bd10605ac9a150f5f48f98f8208dc
Kay Cee <se.kaycee@gmail.com>
2023-04-06 07:35:43 +0100

    [Update] add 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Distribute an archive to web servers using the function do_deploy '''
+from datetime import datetime
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['54.90.40.0', '54.90.23.41']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    ''' Deploy the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        file_name = os.path.basename(archive_path)
+        dir_name = file_name.replace('.tgz', '')
+        releases_path = '/data/web_static/releases/{}/'.format(dir_name)
+        tmp_path = '/tmp/' + file_name
+
+        put(archive_path, tmp_path)
+        run('mkdir -p ' + releases_path)
+        run('tar -xzf {} -C {}'.format(tmp_path, releases_path))
+        run('rm ' + tmp_path)
+        run('mv {}web_static/* {}'.format(releases_path, releases_path))
+        run('rm -rf {}web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
+        return False
