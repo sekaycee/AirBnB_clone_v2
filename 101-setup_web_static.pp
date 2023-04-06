a452dfaabd125565849e24fbf43946edfce38024
Kay Cee <se.kaycee@gmail.com>
2023-04-06 09:08:03 +0100

    [Fix] debug 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,37 +3,37 @@
 exec {'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
-  before   => Exec['install and start Nginx'],
+  before   => Exec['install and start nginx']
 }
 
 exec {'install and start nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx && sudo service nginx start',
-  before   => Exec['make directories'],
+  before   => Exec['make directories']
 }
 
 exec {'make directories':
   provider => shell,
   command  => 'sudo mkdir -p /data/web_static/releases/test/ && sudo mkdir /data/web_static/shared/',
-  before   => Exec['add mock html'],
+  before   => Exec['add mock html']
 }
 
 exec {'add mock html':
   provider => shell,
   command  => 'sudo echo "HBNB Webserver Test" > /data/web_static/releases/test/index.html',
-  before   => Exec['create symbolic link'],
+  before   => Exec['create symbolic link']
 }
 
 exec {'creat symbolic link':
   provider => shell,
   command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => Exec['put location'],
+  before   => Exec['put location']
 }
 
 exec {'put location':
   provider => shell,
   command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
-  before   => Exec['restart nginx'],
+  before   => Exec['restart nginx']
 }
 
 exec {'restart nginx':
@@ -46,5 +46,5 @@ file {'/data/':
   ensure  => directory,
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  recurse => true,
+  recurse => true
 }
