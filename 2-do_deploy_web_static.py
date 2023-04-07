4eaedb5c4ec2c202db4eb7f17bd1e84348fd8adb
Gifftybabe <ulokangozi@gmail.com>
2023-04-06 18:05:56 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,35 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-
+''' Distribute an archive to web servers using the function do_deploy '''
 from datetime import datetime
-from fabric.api import *
+from fabric.api import env, put, run
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
-env.user = "ubuntu"
-
-
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+env.hosts = ['54.90.40.0', '54.90.23.41']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+    ''' Deploy the static files to the host servers
+    Args:
+        archive_path (str): The path to the archived static files
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        file_name = os.path.basename(archive_path)
+        dir_name = file_name.replace('.tgz', '')
+        releases_path = '/data/web_static/releases/{}/'.format(dir_name)
+        tmp_path = '/tmp/' + file_name
 
-        print("New version deployed!")
+        put(archive_path, tmp_path)
+        run('mkdir -p ' + releases_path)
+        run('tar -xzf {} -C {}'.format(tmp_path, releases_path))
+        run('rm ' + tmp_path)
+        run('mv {}web_static/* {}'.format(releases_path, releases_path))
+        run('rm -rf {}web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-
-    return False
+    except Exception:
+        return False
