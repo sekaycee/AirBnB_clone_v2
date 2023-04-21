6e44bf5f6f11c3c296c594f5592280b55b1285d4
Kay Cee <se.kaycee@gmail.com>
2023-04-21 19:40:17 +0100

    [Update] refactor 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    ''' Call with /route.. display HELLO HBNB '''
+    ''' Call with / route.. display HELLO HBNB '''
     return ('Hello HBNB!')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
