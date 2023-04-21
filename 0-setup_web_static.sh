5f96261ae6d6d8f7802e3c45b33c721aa2933bc5
Kay Cee <se.kaycee@gmail.com>
2023-04-21 19:41:09 +0100

    [Fix] debug code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ apt -y install nginx
 service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir /data/web_static/shared/
-echo 'HBNB Webserver Test' > /data/web_static/releases/test/index.html
+echo 'Holberton School Test' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -20,7 +20,7 @@ exec {'make directories':
 
 exec {'add mock html':
   provider => shell,
-  command  => 'sudo echo "HBNB Webserver Test" > /data/web_static/releases/test/index.html',
+  command  => 'sudo echo "Holberton School Test" > /data/web_static/releases/test/index.html',
   before   => Exec['create symbolic link']
 }
 
@@ -32,7 +32,7 @@ exec {'create symbolic link':
 
 exec {'put location':
   provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default',
   before   => Exec['restart nginx']
 }
 
