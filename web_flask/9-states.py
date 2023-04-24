cee0bd1a098965cd85f52ca43cf1e4c52062590b
Kay Cee <se.kaycee@gmail.com>
2023-04-24 05:55:24 +0100

    fix: fix finally task #10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -20,10 +20,9 @@ def states_id(id=None):
     if not id:
         return (render_template('7-states_list.html', states=states))
 
-    #key = 'State.{}'.format(id)
-    for state in states.values():
-        if state.id == id:
-            return (render_template('9-states.html', state=state))
+    key = 'State.{}'.format(id)
+    if key in states:
+        return (render_template('9-states.html', state=state))
     return (render_template('9-states.html', state=None))
 
 
